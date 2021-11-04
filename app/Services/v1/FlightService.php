d1c917addfefedadce418c358096ed4a981b5399
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-11-04 14:26:36 +0200

    filterFlights protected function implemented

--- app/Services/v1/FlightService.php
+++ app/Services/v1/FlightService.php
@@ -6,7 +6,19 @@ use App\Flight;
 
 class FlightService{
     public function getFlights(){
-        return Flight::all(); 
+        return $this->filterFlights(Flight::all()); 
+    }
+
+    protected function filterFlights($flights){
+        $data = [];
+        foreach ($flights as $flight){
+            $entry = [
+                'flightNumber' => $flight->flightNumber,
+                'status' => $flight->status
+            ];
+            $data[] = $entry;
+        }
+        return $data;
     }
 }
 
